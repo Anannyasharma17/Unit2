cf0ac580e54b657422bbec4565b32a76a43a7c5e
Anannya Sharma <Anannyasharma8300@gmail.com>
2022-04-25 18:52:32 +0530

    Initial commit

---
+++ project Unit 2/slide.js
@@ -0,0 +1,21 @@
+function start(){
+
+    let images_arr = JSON.parse(localStorage.getItem("images"))
+
+    let imagee = document.getElementById("imagee")
+
+    let i=0;
+
+    setInterval(fucntion () {
+        
+
+        imagee.src
+
+        
+        i= i+1
+
+
+    }, 2000)
+
+    
+}
